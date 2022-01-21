5fd6dfc3234577d46ab636fcc8cd371c03e1fabc
Guillaume MECHAT <mechat_g@etna-alternance.net>
2022-01-21 15:39:43 +0100

    Update time.groovy

--- vars/time.groovy
+++ vars/time.groovy
@@ -2,7 +2,7 @@ import groovy.time.*
 
 def duration() {
   def timeStart = new Date()
-  // Some code you want to time
+  // Some code you want to time 
   def timeStop = new Date()
   //TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
 
