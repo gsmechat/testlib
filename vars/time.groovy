28cde6f55f1d5753dcabcd6770e537a3a6658478
Guillaume MECHAT <mechat_g@etna-alternance.net>
2022-01-21 15:26:26 +0100

    Update and rename log_info.groovy to time.groovy

---
+++ vars/time.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+import groovy.time.*
+
+def duration() {
+  def timeStart = new Date()
+  // Some code you want to time
+  def timeStop = new Date()
+  TimeDuration duration = TimeCategory.minus(timeStop, timeStart)
+
+  return duration
+}
